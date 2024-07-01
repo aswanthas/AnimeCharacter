ff39959ff9e2913ddf8bc81d1f25f1c92342752c
Aswa_nth* <aswanthas124@gmail.com>
2024-07-01 19:06:34 +0530

    Initial Commit

---
+++ AnimeCharacter/AnimeCharacterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeCharacterApp.swift
+//  AnimeCharacter
+//
+//  Created by Aswanth K on 01/07/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimeCharacterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
